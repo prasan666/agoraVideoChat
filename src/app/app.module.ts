e691cbbe59b6460d246919605fea152b3e49cdd7
prasanna7 <prasanna@livbim.io>
2021-01-16 11:24:10 +0530

    added agora plugin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularAgoraRtcModule, AgoraConfig } from 'angular-agora-rtc'; // Add
+
+
+const agoraConfig: AgoraConfig = {
+  AppID: '3ddcbd20181445629553a11daf545932',
+};
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    AngularAgoraRtcModule.forRoot(agoraConfig) ,// Add
     AppRoutingModule
   ],
   providers: [],
